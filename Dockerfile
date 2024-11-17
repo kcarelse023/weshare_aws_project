de2a0c630162dbe08ae817d198b001cd98c5a7b9
Kurt Carelse <kcarelse023@student.wethinkcode.co.za>
2024-11-17 02:37:12 +0200

    clean2

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,19 @@
-FROM public.ecr.aws/docker/library/node:buster-slim
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
+# Step 1: Use a Maven image to build the project
+FROM maven:3.9.4-eclipse-temurin-17 AS build
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the entire project into the container
 COPY . .
+
+# Compile the application (skip tests if necessary)
+RUN mvn clean package -DskipTests
+RUN ls -a
+
+# Expose the port the application will run on
 EXPOSE 80
-CMD ["node", "server.js"]
+
+# Step 2: Don't run the application explicitly here
+# Comment out or remove the CMD line
+CMD [ "java", "-jar", "target/weshare-mvc-exercise-1.0-SNAPSHOT-jar-with-dependencies.jar" ]
---
+++ src/main/java/weshare/server/Routes.java
@@ -0,0 +1,36 @@
+package weshare.server;
+
+import weshare.controller.*;
+
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.post;
+
+public class Routes {
+    public static final String LOGIN_PAGE = "/";
+    public static final String LOGIN_ACTION = "/login.action";
+    public static final String LOGOUT = "/logout";
+    public static final String EXPENSES = "/expenses";
+    public static final String NEWEXPENSE = "/newexpense";
+    public static final String ADDEXPENSE = "/expense.action";
+    public static final String PAYMENT_REQUEST = "/paymentrequest";
+    public static final String SENT = "/paymentrequests_sent";
+    public static final String RECEIVED = "/paymentrequests_received";
+    public static final String SUBMIT_PAYMENT_REQUEST = "/paymentrequest.action";
+    public static final String PAY_PAYMENT_REQUEST = "/payment.action";
+
+
+    public static void configure(WeShareServer server) {
+        server.routes(() -> {
+            post(LOGIN_ACTION,  PersonController.login);
+            get(LOGOUT,         PersonController.logout);
+            get(EXPENSES,           ExpensesController.view);
+            post(NEWEXPENSE, ExpensesController.newExpenses);
+            post(ADDEXPENSE, ExpensesController.addExpenses);
+            get(PAYMENT_REQUEST, PaymentRequestController.viewPaymentRequest);
+            get(SENT, PaymentRequestController.viewSent);
+            get(RECEIVED, PaymentRequestController.viewReceived);
+            post(SUBMIT_PAYMENT_REQUEST, PaymentRequestController.sendPaymentRequest);
+            post(PAY_PAYMENT_REQUEST, PaymentRequestController.payPaymentRequest);
+        });
+    }
+}
